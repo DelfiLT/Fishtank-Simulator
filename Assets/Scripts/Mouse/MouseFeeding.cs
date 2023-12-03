5ef017628394eb14651b6bf036c3b05de64ca5ae
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-12-03 17:19:51 -0300

    Instanciado de comida en ubicación random

--- Assets/Scripts/Mouse/MouseFeeding.cs
+++ Assets/Scripts/Mouse/MouseFeeding.cs
@@ -1,17 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseFeeding : MouseState
 {
     public override void ClickAction()
     {
-        GameObject food = FoodPool.Instance.GetPooledFood();
-
-        if (food != null)
+        StartCoroutine(SpawnFood());
+    }
+    IEnumerator SpawnFood()
+    {
+        for (int i = 0; i < (Random.Range(3, 7)); i++)
         {
-            food.transform.position = MousePosition.Instance.worldPosition;
-            food.SetActive(true);
+            GameObject food = FoodPool.Instance.GetPooledFood();
+
+            float randomX = Random.Range(0, 3);
+            float randomY = Random.Range(0, 3);
+
+            if (food != null)
+            {
+                Vector3 spawnPosition = new Vector3(MousePosition.Instance.worldPosition.x + randomX, MousePosition.Instance.worldPosition.y + randomY, MousePosition.Instance.worldPosition.z);
+                food.transform.position = spawnPosition;
+                food.SetActive(true);
+            }
+            yield return null;
         }
     }
 }
--- Assets/Scripts/Mouse/MouseIdle.cs
+++ Assets/Scripts/Mouse/MouseIdle.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class MouseIdle : MouseState
 {
     public override void ClickAction()
