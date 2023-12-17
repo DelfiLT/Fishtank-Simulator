4ab30050e56486579ed8208a07e65d300da1a68d
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-12-16 21:52:40 -0300

    Placeholder food box follows the cursor

---
+++ Assets/Scripts/Mouse/FoodBoxManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FoodBoxManager : MonoBehaviour
+{
+    [SerializeField] GameObject foodBox;
+
+    private void Awake() => FoodToggler.onToggleActivated += Toggle;
+
+    void Toggle()
+    {
+        if (FoodToggler.Instance.foodGrabbed)
+        {
+            foodBox.SetActive(true);
+        }
+        else
+        {
+            foodBox.SetActive(false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foodBox.transform.position = new Vector3(MousePosition.Instance.worldPosition.x, 4.3f, MousePosition.Instance.worldPosition.z);
+    }
+}
